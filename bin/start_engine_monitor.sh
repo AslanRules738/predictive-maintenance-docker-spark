465633a2bff71094e9914f30ad69460ab7cdcc45
Braedon Sweeley <168577765+AslanRules738@users.noreply.github.com>
2024-11-21 02:58:22 -0500

    Update start_engine_monitor.sh

--- bin/start_engine_monitor.sh
+++ bin/start_engine_monitor.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 RLU_THRESHOLD=$1
+DIR=/media/mike/project
 
-spark-submit --master local[2] --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0 --jars jars/spark-streaming-kafka-0-10_2.11-2.4.0.jar kafka/engine_cycle_consumer_struct.py kafka:9092 engine-stream engine-alert -w 30 -s 30 -r ${RLU_THRESHOLD} 
+spark-submit --master local[2] --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0 --jars ${DIR}/jars/spark-streaming-kafka-0-10_2.11-2.4.0.jar ${DIR}/kafka/engine_cycle_consumer_struct.py kafka:9092 engine-stream engine-alert -w 30 -s 30 -r ${RLU_THRESHOLD} 
