5692fae9497c8a75c4f2edda6bb8826083a395f5
Braedon Sweeley <168577765+AslanRules738@users.noreply.github.com>
2024-11-21 02:56:46 -0500

    Update start_engine_sim.sh

--- bin/start_engine_sim.sh
+++ bin/start_engine_sim.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 RPS=$1
-DIR=/shared/project
+DIR=/media/mike/project
 
 echo "rps= $RPS"
 python ${DIR}/kafka/engine_cycle_producer.py kafka:9092 engine-stream -s ${DIR}/data/test_x.csv -r ${RPS} 
